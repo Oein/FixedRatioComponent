8bb56da412de0c473e348e8e137e192794cee074
Oein <oein0219@gmail.com>
2023-09-20 17:50:03 +0900

    add readme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,14 @@
 </script>
 
 <div class="wrapper">
-	<FixedRatio />
+	<FixedRatio
+		width_radio={21}
+		height_radio={9}
+		wrapperSyle="background: black;"
+		ratioStyle="background: coral;"
+	>
+		<div>It's me!</div>
+	</FixedRatio>
 </div>
 
 <style lang="scss">
